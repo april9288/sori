88dd631d9ae779bc2a0a895f2585f345d7051262
Jongho Kim <april9288@gmail.com>
2018-10-04 23:59:23 -0700

    Updates

---
+++ precache-manifest.9d36ff60b25eebfe82f974b3f3e9e185.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a06e2e38a9e63c2c169f",
+    "url": "/sori/static/js/runtime~main.a06e2e38.js"
+  },
+  {
+    "revision": "e7555db90df4b9595d2f",
+    "url": "/sori/static/js/main.e7555db9.chunk.js"
+  },
+  {
+    "revision": "924458280a884ee4f0cf",
+    "url": "/sori/static/js/1.92445828.chunk.js"
+  },
+  {
+    "revision": "e7555db90df4b9595d2f",
+    "url": "/sori/static/css/main.e68b8d47.chunk.css"
+  },
+  {
+    "revision": "fa781cf6d07beb0e7d7dfe2f98200ca6",
+    "url": "/sori/index.html"
+  }
+];
